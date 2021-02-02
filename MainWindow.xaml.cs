009ac5660ca7bf8e704eac6865cdae16afe4c63a
Bartłomiej Kot <bartlomiej.kot@microsoft.wsei.edu.pl>
2021-02-02 19:30:39 +0100

    dodanie okna logowania

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.SqlClient;
+
 
 namespace Projekt
 {
@@ -20,9 +22,52 @@ namespace Projekt
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        
+        
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            pobierzDane();
+        }
+        
+        public void pobierzDane()
+        {
+            //pobierz dane logowania z formularza i przypisz
+            string mojePolaczenie =
+                "SERVER=" + Nservera.Text + ";" +
+                "DATABASE=" + NazwaDB.Text + ";" +
+                "UID=" + Nużytkownika.Text + ";" +
+                "PASSWORD=" + Hużytkownika.Password + ";";
+
+            SqlConnection polaczenie = new SqlConnection(mojePolaczenie);
+
+            try
+            {
+                polaczenie.Open();
+                var Okno = new Okno();
+                Okno.ShowDialog();
+
+                //using (SqlCommand cmdSel = new SqlCommand(sql, polaczenie))
+                //{
+
+                //    DataTable dt = new DataTable();
+
+                //    SqlDataAdapter da = new SqlDataAdapter(cmdSel);
+                //    da.Fill(dt);
+
+                    
+                //}
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Błąd logowania");
+            }
+
         }
     }
 }
